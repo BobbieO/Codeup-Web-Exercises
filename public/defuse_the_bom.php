9c7a92383d5732de73a1a52bdb3e0aad55bd29f5
BobbieO <bobbie2504@msn.com>
2016-03-22 10:40:07 -0500

    Completed exercise; notes re DOM and BOM; shorter function call notation

--- public/defuse_the_bom.php
+++ public/defuse_the_bom.php
@@ -1,3 +1,24 @@
+<!-- Window IS the BOM, and DOM is document. Everything belongs to BOM, DOM is a child of BOM.
+setInterval() sets the interval between repetitions of an action set;
+clearInterval() stops the interval repetitions;
+setTimeout() allows code to be executed after a specified delay;
+clearTimeout() cancels the setTimeout;
+CMD-shift-R is hard refresh from server; little circle arrow is refresh from cache;
+
+Example of cleaner function call within setTimeout:
+
+function alertMessage() {
+    alert("Here is a delayed hello");
+}
+
+var timeoutId = setTimeout(alertMessage, delay);
+
+No parenthesis needed on line 14's alertMessage bc the setTimeout calls it.
+
+ -->
+}
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -21,14 +42,24 @@
                 document.getElementById('timer').innerHTML = detonationTimer;
             }
 
+            //testing stopping at 2 sec with an alert msg
+        // {
+        //     if (detonationTimer == 2) {
+        //         alert("Movie Save!");
+        //         clearInterval(timer);
+        //     }
+        // }
+
             detonationTimer--;
         }
+
+        var timer = setInterval(updateTimer, 1000);
 
         // TODO: When this function runs, it needs to
         // cancel the interval/timeout for updateTimer()
         function defuseTheBOM()
         {
-
+            clearInterval(timer);
         }
 
         // Don't modify anything below this line!
