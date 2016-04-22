82dcebfe806bcd7d3a85e4f98934fc11e5aaee66
BobbieO <bobbie2504@msn.com>
2016-04-22 16:22:45 -0500

    Generated random adj and noun functions

--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,15 +1,23 @@
 <?php
 
-$adjectives = ["hearty", "strong", "lovely", "diseased", "spotty", "mutated", "fast", "slow", "angry", "cuddly", "unusual", "smelly", "adorable", "intelligent", "hideous", "cute"];
-
-$nouns = ["cows", "boars", "rats", "ogres", "rocs", "dragons", "dwarfs", "gnomes", "goblins", "undead", "elves", "bears", "fish"];
-
 function randomize() 
 {
-    //pick random adjective
+
+    $adjectives = ["hearty", "strong", "lovely", "diseased", "spotty", "mutated", "fast", "slow", "angry", "cuddly", "unusual", "smelly", "adorable", "intelligent", "hideous", "cute"];
+
+    $nouns = ["cows", "boars", "rats", "ogres", "rocs", "dragons", "dwarfs", "gnomes", "goblins", "undead", "elves", "bears", "fish"];
+
+    //pick random index of adjectives array
+    $randomAdj = array_rand($adjectives);
+
+    echo $adjectives[$randomAdj];
+
     //pick random noun
     //combine them
     //output to page
+
 }
+
+var_dump(randomize());
 
 ?>
