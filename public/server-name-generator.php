e4607da600ae87b7558d76d157ec255d330ee446
BobbieO <bobbie2504@msn.com>
2016-04-22 15:22:31 -0500

    Initial commit of exercise. Added two arrays: adjectives and nouns, skeleton of function.

---
+++ public/server-name-generator.php
@@ -0,0 +1,15 @@
+<?php
+
+$adjectives = ["hearty", "strong", "lovely", "diseased", "spotty", "mutated", "fast", "slow", "angry", "cuddly", "unusual", "smelly", "adorable", "intelligent", "hideous", "cute"];
+
+$nouns = ["cows", "boars", "rats", "ogres", "rocs", "dragons", "dwarfs", "gnomes", "goblins", "undead", "elves", "bears", "fish"];
+
+function randomize() 
+{
+    //pick random adjective
+    //pick random noun
+    //combine them
+    //output to page
+}
+
+?>
