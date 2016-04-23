c0687f66a4c933123f983de4d244afa989bc7027
BobbieO <bobbie2504@msn.com>
2016-04-23 15:52:07 -0500

    Added js folder and notes for javascript additions

---
+++ public/js/server-name-generator.js
@@ -0,0 +1,16 @@
+// page loads with castle, king-bed, desk-miniScroll, warrior
+
+//small delay
+
+//king speaks "tired, your task is on desk"
+
+//king text changes to "zzzz"
+
+//animation on warrior moves to desk
+
+//text alert "click on scroll"
+
+//javascript changes view to big scroll background with php
+    //generator thing
+
+    
