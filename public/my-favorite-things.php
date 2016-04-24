b6f1c52e7e67bd86809914707cbc8ed34de63e3d
BobbieO <bobbie2504@msn.com>
2016-04-24 13:21:28 -0500

    Initial commit of php/html and css files for exercise. Base HTML and css styling completed: table structure, temp contents, table positioning, background and cell colors, font colors.

---
+++ public/my-favorite-things.php
@@ -0,0 +1,50 @@
+<?php
+
+$favoriteThings = ["dogs", "pizza", "books", "key lime pie", "music", "science fiction", "fantasy", "winter"];
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Favorite Things</title>
+
+    <!-- my css -->
+    <link rel="stylesheet" type="text/css" href="/css/my-favorite-things.css">
+
+</head>
+<body>
+
+<div>
+    <h2>My Favorite Things</h2>
+    <div>
+        <table>
+            <tr>
+                <th>Some of My Favorite Things, in No Particular Order</th>
+            </tr>
+
+            <tr>
+                <td>another thing</td>
+            </tr>
+
+            <tr>
+                <td>and another thing</td>
+            </tr>
+
+            <tr>
+                <td>something else</td>
+            </tr>
+
+            <tr>
+                <td>and this thing</td>
+            </tr>
+
+            <tr>
+                <td>this one too</td>
+            </tr>
+
+        </table>
+    </div>
+</div>
+</body>
+</html>
