221efda25359eaf57869fc9376a0e43df2507a3b
BobbieO <bobbie2504@msn.com>
2016-04-25 10:06:31 -0500

    Fixed table row-data output so that each item appears in its own row

--- public/my-favorite-things.php
+++ public/my-favorite-things.php
@@ -1,33 +1,6 @@
 <?php
 
-function randomize() {
-
-
-    $favoriteThings = ["dogs", "pizza", "books", "key lime pie", "music", "science fiction", "fantasy", "winter"];
-
-    //array of indexes of five random things
-    // $fiveRandThings = array_rand($favoriteThings, 5);
-
-    //get values instead of indexes
-    // $fiveRandValues = $favoriteThings[$fiveRandThings];
-
-    //implode array into a string
-    // $fiveRandString = implode(", ", $fiveRandValues);
-
-    // var_dump($fiveRandThings);
-
-//something in the above is broken.
-
-    //returns only the first item.
-    foreach($favoriteThings as $oneThing) {
-        return $oneThing;
-    }
-
-
-}
-
-echo(randomize());
-
+    $favoriteThings = ["dogs", "pizza", "books", "key lime pie", "winter"];
 ?>
 
 <!DOCTYPE html>
@@ -49,25 +22,9 @@ echo(randomize());
                 <th>Some of My Favorite Things, in No Particular Order</th>
             </tr>
 
-            <tr>
-                <td><?php echo(randomize())?></td>
-            </tr>
-
-            <tr>
-                <td>and another thing</td>
-            </tr>
-
-            <tr>
-                <td>something else</td>
-            </tr>
-
-            <tr>
-                <td>and this thing</td>
-            </tr>
-
-            <tr>
-                <td>this one too</td>
-            </tr>
+            <?php foreach($favoriteThings as $favoriteItem) { ?>
+                <tr><td><?php echo $favoriteItem; ?></td></tr>
+            <?php } ?>
 
         </table>
     </div>
