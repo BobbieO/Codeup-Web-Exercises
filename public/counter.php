f884d0d9fc3a19b66ae295adbb982412fa875596
BobbieO <bobbie2504@msn.com>
2016-04-26 15:27:20 -0500

    Completed counter

---
+++ public/counter.php
@@ -0,0 +1,29 @@
+<?php
+
+function pageController()
+{
+    $count = $_GET['count'];
+    return ['count' => $count];
+}
+
+extract(pageController());
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset = "UTF-8">
+    <title>Counter</title>
+</head>
+<body>
+
+<div>
+    <h1><?= $count ?></h1>
+</div>
+
+<a href="?count=<?= $count + 1 ?>">Up</a>
+<a href="?count=<?= $count - 1 ?>">Down</a>
+
+</body>
+</html>
