628825535d6a0946edde47b8076260db5482892e
BobbieO <bobbie2504@msn.com>
2016-04-27 10:55:32 -0500

    initial commit

---
+++ public/authorized.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Authorization</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
---
+++ public/login.php
@@ -0,0 +1,21 @@
+<?php
+
+
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>POST Requests Form</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
