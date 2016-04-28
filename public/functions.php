5607b1aba017b593716dc8da8fcbb51b9f9c0114
BobbieO <bobbie2504@msn.com>
2016-04-28 13:59:18 -0500

    Initial draft of functions completed

--- public/functions.php
+++ public/functions.php
@@ -1,6 +1,28 @@
 <?php
+function pageController() {
+
+    function inputHas($key) {
+        if($key) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function inputGet($key) {
+        if (isset($key['value'])) {
+            return $value;
+        } else {
+            return null;
+        }
+    }
+
+    function escape($input) {
+        return htmlspecialchars(strip_tags($input));
+    }
 
 
+}
 ?>
 
 
@@ -10,6 +32,10 @@
     <title>Functions</title>
 </head>
 <body>
+      <? php require_once '/ping.php'; ?> 
+      <? php require_once '/pong.php'; ?> 
+      <? php require_once '/login.php'; ?> 
 
 </body>
 </html>
+
