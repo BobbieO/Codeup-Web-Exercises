92b046bd1730871d188faebfe7d6af53ca2675fd
BobbieO <bobbie2504@msn.com>
2016-04-29 15:42:07 -0500

    Added text, image, and link to weather map page.

--- public/weathermap.php
+++ public/weathermap.php
@@ -25,8 +25,35 @@
     <main>
         <div class="center container">
             <h1>Weather Map</h1>
-            <p>Explanation Here</p>
+            <p>
+                An interactive weather map using JavaScript and jQuery for function, and Bootstrap CSS for styling. The map integrates Google Maps API and Open Weather API together to retrieve the weather from any given location in the world.
+            </p>
+
+            <p>
+                This map features a Google Map with a draggable marker. A listener event registers the latitutde and longitude of the end location of the marker, which is then used to gather the weather via the Open Weather API. The information is then dynamically displayed below the map.
+            </p>
+
         </div>
+
+        <div>
+            <div class="container row">
+                <div class="col s6 instrImg">
+                <h3 class="center">Map Instructions</h2>
+                    <ol>
+                        <li>Click and hold on the map marker.</li>
+                        <li>Drag to the desired location.</li>
+                        <li>The + and - will zoom the map in and out.</li>
+                    </ol>
+                </div>
+                <div class="col s6 instrImg">
+                    <h3 class="center">Click to View</h3>
+                    <div class="center">
+                        <a href="http://bobbieo.github.io/weather_map.html" target="_blank"><img src="/img/screenShotWeather.png"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </main>
 
     <footer>
