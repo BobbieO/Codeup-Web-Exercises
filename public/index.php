279589543e6807857ada0c82552da1a0e522b47a
BobbieO <bobbie2504@msn.com>
2016-04-29 18:14:57 -0500

    Edited landing page text. Added css for font size.

--- public/index.php
+++ public/index.php
@@ -25,12 +25,10 @@
     <main>
 
     <div class="center container">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p class="bigger">Welcome to my site!</p>
+        <p class="bigger">
+            I'm a San Antonio-based web developer using both front-end and back-end technologies. I'm currently working with HTML5, CSS3, JavaScript and jQuery, and PHP. 
+        </p>
     <div>
 
     </main>
