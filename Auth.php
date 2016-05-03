b1e16a8f239dd356792ee77b20199148b86cdd36
BobbieO <bobbie2504@msn.com>
2016-05-03 15:11:10 -0500

    Added static functions for log processes

--- Auth.php
+++ Auth.php
@@ -6,29 +6,46 @@ class Auth
 {
     public static $password = '$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm';
 
+    //to check if username and pass is correct
     public static function attempt($username, $password)
     {
-        if($username == 'guest' && $password == hashed password)
-                Log("User {$username} logged in.");
-        else {
-            Log("User {$username} failed to log in!");
-        }
-        
+        //generate new instance of Log
+        $Log = new Log;
+
+        //if pass and user both correct, log success alert
+        if($username == 'guest' && $password == password_verify($password)) {
+            $Log->info("User {$username} logged in.");
+
+        //if not, log error alert
+        } else {
+            $Log->error("User {$username} failed to log in!");
+        }    
     }
 
+    //to check if user is logged in already
     public static function check()
     {
-
+        if(isset($_SESSION["logged_in_user"])) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    //to return username
     public static function user()
     {
-
+        return $_SESSION["logged_in_user"];
     }
 
+    //to log out/cease functions
     public static function logout()
     {
+        // clear $_SESSION array
+        session_unset();
 
+        // delete session data on the server and send the client a new cookie
+        session_regenerate_id(true);  
     }
 
 
