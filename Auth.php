ac3580f58485c0859b6f253c9085dfb3afe1722c
BobbieO <bobbie2504@msn.com>
2016-05-03 17:25:01 -0500

    Edited instance variable name

--- Auth.php
+++ Auth.php
@@ -10,17 +10,17 @@ class Auth
     public static function attempt($username, $password)
     {
         //generate new instance of Log
-        $Log = new Log;
+        $log = new Log;
 
         //if pass and user both correct, log success alert
         if($username == 'guest' && password_verify($password, self::$password)) {
             $_SESSION["logged_in_user"] = $username;
-            $Log->info("User {$username} logged in.");
+            $log->info("User {$username} logged in.");
             return true;
 
         //if not, log error alert
         } else {
-            $Log->error("User {$username} failed to log in!");
+            $log->error("User {$username} failed to log in!");
             return false;
         }    
     }
