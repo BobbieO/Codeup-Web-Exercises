34e6e6a8e686af0377f7e8f43e64d7454cd249d3
BobbieO <bobbie2504@msn.com>
2016-05-12 10:13:13 -0500

    Added credentials file, added credentials file to gitignore. Edited pdotest to remove sensitive info.

--- pdo_test.php
+++ pdo_test.php
@@ -1,9 +1,5 @@
 <?php
-//define constants to sub into dbc
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'employees');
-define('DB_USER', 'vagrant');
-define('DB_PASS', 'vagrant');
+
 
 
 REQUIRE 'db_connect.php';
