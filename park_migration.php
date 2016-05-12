08c229b8540217b1578254242bb0cbad7eed8077
BobbieO <bobbie2504@msn.com>
2016-05-12 13:14:10 -0500

    Added create table query

--- park_migration.php
+++ park_migration.php
@@ -15,5 +15,17 @@ $query = 'DROP TABLE IF EXISTS national_parks';
 //run the above query
 $dbc->exec($query);
 
+$query = 'CREATE TABLE national_parks (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    location VARCHAR(100),
+    date_established DATE,
+    area_in_acres DOUBLE,
+    PRIMARY KEY (id)
+)';
+
+//run the above query
+$dbc->exec($query);
+
 ?>
 
