d58ebd419bf89f3715cc3d893a7c816c34394389
BobbieO <bobbie2504@msn.com>
2016-05-13 14:51:27 -0500

    Added pagination, limited "buttons" appearing on pages where not needed.

--- public/national_parks.php
+++ public/national_parks.php
@@ -7,18 +7,17 @@ function pageController($dbc) {
 
     $data = [];
 
-    $stmt = $dbc->query('SELECT * FROM national_parks');
+    $data['page'] = Input::has('page') ? Input::get('page') : 1;
 
+    $offset = ($data['page'] - 1) * 4; 
+
+    $stmt = $dbc->query('SELECT * FROM national_parks LIMIT 4 OFFSET ' . $offset);
+    
     $data['parks'] = $stmt->fetchALL(PDO::FETCH_ASSOC);
 
-    $data['parks'] = $stmt->
-
     return $data;
-
 }
-
     extract(pageController($dbc));
-
 ?>
 
 <!DOCTYPE html>
@@ -27,6 +26,7 @@ function pageController($dbc) {
 
     <meta charset="utf-8">
     <title>National Parks</title>
+    <p>You are on page <?=$page;?> </p>
 
     <!-- my css -->
     <link rel="stylesheet" type="text/css" href="/css/national_parks.css">
@@ -43,7 +43,6 @@ function pageController($dbc) {
                 <th>Location</th>
                 <th>Date Established</th>
                 <th>Acreage</th>
-
             </tr>
 
             <?php foreach($parks as $park) : ?>
@@ -57,7 +56,15 @@ function pageController($dbc) {
     </div>
 </div>
 
+<footer class="footer"> 
+    <?php if($page>=2): ?> 
+        <a href="?page=<?= $page - 1 ?>" class="btn btn-default">Previous</a>
+    <?php endif; ?>
 
+    <?php if($page < count($parks) ): ?>
+        <a href="?page=<?= $page + 1 ?>" class="btn btn-default">Next</a>
+    <?php endif; ?>
+</footer>
 
 </body>
 </html>
