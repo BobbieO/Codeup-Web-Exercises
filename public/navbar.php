405e06f058467a6932abfe489a445b7f89b2de7c
BobbieO <bobbie2504@msn.com>
2016-05-14 12:34:28 -0500

    Edited navbar to place all projects in one dropdown.

--- public/navbar.php
+++ public/navbar.php
@@ -6,33 +6,22 @@
   <li><a href="/weathermap.php">Weather Map</a></li>
   <li><a href="/basicCalculator.php">Basic Calculator</a></li>
   <li class="divider"></li>
-  <li><a href="/index.php">Future Projects</a></li>
-</ul>
 
-<ul id="dropdownPhp" class="dropdown-content">
   <li><a href="/randomQuestGen.php">Random Quest Generator</a></li>
   <li><a href="/basicPingPong.php">Basic Ping Pong</a></li>
   <li class="divider"></li>
+
   <li><a href="/index.php">Future Projects</a></li>
 </ul>
 
 <nav>
     <div class="nav-wrapper grey darken-3">
         <a href="/index.php" class="brand-logo">BHO</a>
-
-        <!-- mobile view not working -->
-        <!-- <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a> -->
-
-        <ul id="nav-mobile" class="text right hide-on-med-and-down">
+        <ul class="text right"> 
             <li><a href="/index.php">Contact Me</a></li>
             <li><a href="/index.php">Resume</a></li>
-
             <!-- Dropdown Trigger -->
-            <li><a class="dropdown-button" href="#!" data-activates="dropdownJava">JavaScript Projects<i class="material-icons right">arrow_drop_down</i></a></li>
-
-            <!-- Dropdown Trigger -->
-            <li><a class="dropdown-button" href="#!" data-activates="dropdownPhp">PHP Projects<i class="material-icons right">arrow_drop_down</i></a></li>
-
+            <li><a class="dropdown-button" href="#!" data-activates="dropdownJava">Projects<i class="material-icons right">arrow_drop_down</i></a></li>
         </ul>
     </div>
   </nav>
