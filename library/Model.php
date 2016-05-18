66c2bafe688128fa9cef24abc7de717112612513
BobbieO <bobbie2504@msn.com>
2016-05-18 11:55:42 -0500

    Added get and set functions

--- library/Model.php
+++ library/Model.php
@@ -1,5 +1,26 @@
 <?php
 
+class Model 
+{
+    private $attributes = [];
+
+    //magic setter to populate attrib array
+    public function __set($name, $value)
+    {
+        //set key and value pairs
+        $this->attribute[$name] = $value;
+    }
+
+    //magic getter to retrieve info
+    public function __get($name)
+    {
+        //if key exists, return value
+        if(array_key_exists($name, $this->attributes)) {
+            return $this->attribute[$name];
+        } 
+        return null;
+    }
+}
 
 
 ?>
