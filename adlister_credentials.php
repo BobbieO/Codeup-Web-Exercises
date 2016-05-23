4319d9d5fdf28d4c8b6edc65b24fc5f178fa6108
BobbieO <bobbie2504@msn.com>
2016-05-23 14:12:04 -0500

    Deleted unneeded file.

---
+++ adlister_credentials.php
@@ -0,0 +1,9 @@
+<?php
+
+//define constants to sub into dbc
+define('DB_HOST', '127.0.0.1');
+define('DB_NAME', 'adlister_db');
+define('DB_USER', 'bobbieo');
+define('DB_PASS', 'rhiannon13');
+
+?>
